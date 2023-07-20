eb0a8398dfd908c93a12d59caf75e1907624649e
abinaya15meenatchisundaram <johndoe@example.com>
2023-07-20 11:58:49 +0530

    Casting

--- rust_doc/src/main.rs
+++ rust_doc/src/main.rs
@@ -7,11 +7,36 @@ use std::cmp::Ordering;
 
 
 
-fn main() {
-    println!("What is your name?");
-    let mut name: String= String ::new();
-    let greeting: &str = "Nice to meet you";
-    io::stdin().read_line(&mut name)
-        .expect("Didn't Receive Input");
-    println!("Hello {}! {}", name.trim_end(), greeting);
+fn main(){
+    enum Day{
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    impl  Day {
+        fn is_weekend(&self) ->bool{
+            match self{
+                Day::Saturday | Day::Sunday => true,
+                _=> false
+            }
+        }
+    }
+    let today:Day=Day::Monday;
+    match today{
+        Day::Monday=>println!("Everyone Hates Monday"),
+        Day::Tuesday =>println!("Donut day"),
+        Day::Wednesday => println!("Hump Day"),
+        Day::Thursday =>println!("Pay DAy"),
+        Day::Friday=>println!("Almost Weekend"),
+        Day::Saturday =>println!("Weekend"),
+        Day::Sunday =>println!("Weekend"),
+    }
+
+    println!("Is today a weekend {}",today.is_weekend());
+
 }

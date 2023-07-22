6d151b02e1ce0f0ac533b59d6674d5e0a2228ab2
abinaya15meenatchisundaram <abinaya.meenatchisundharam@gmail.com>
2023-07-22 21:46:04 +0530

    trait

--- Rust_doc/target/debug/deps/traits-e96756a08c264ba6.d
+++ Rust_doc/target/debug/deps/traits-e96756a08c264ba6.d
@@ -1,5 +1,5 @@
-C:\Users\91908\Rust\Rust-Documentation\Rust_doc\target\debug\deps\traits-e96756a08c264ba6.rmeta: src\bin\traits.rs
+c:\Users\91908\Rust\Rust-Documentation\Rust_doc\target\debug\deps\traits-e96756a08c264ba6.rmeta: src\bin\traits.rs
 
-C:\Users\91908\Rust\Rust-Documentation\Rust_doc\target\debug\deps\traits-e96756a08c264ba6.d: src\bin\traits.rs
+c:\Users\91908\Rust\Rust-Documentation\Rust_doc\target\debug\deps\traits-e96756a08c264ba6.d: src\bin\traits.rs
 
 src\bin\traits.rs:

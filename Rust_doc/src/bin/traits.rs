b4b1c76de8652563225d4936f65c7dc9111c7ecc
abinaya15meenatchisundaram <abinaya.meenatchisundharam@gmail.com>
2023-07-22 21:44:12 +0530

    traits

---
+++ Rust_doc/src/bin/traits.rs
@@ -0,0 +1,36 @@
+#![allow(unused)]
+pub struct NewArticle{
+    pub author :String,
+    pub headline:Stinrg,
+    pub content:String,
+}
+
+impl Summary for NewArticle{
+    fn summarize(&self)-> String{
+        format!("{}, by {}",self.headline,self.author)
+    }
+}
+pub struct Tweet{
+    pub username:String,
+    pub content:Stirng,
+    pub replly:bool,
+    pub retweet:bool,
+}
+
+impl Summary for Tweet{
+    fn summarize(&self)->String{
+        format("{}, by {}",self.username,self.author)
+    }
+}
+pub trait Summary{
+    fn summarize(&self) ->String{
+        String::from("(Read more...)")
+    }
+}
+
+fn main(){
+    let tweet=Tweet{
+        username:String::from("@Abinaya"),
+        content:String::form("Hello World"),
+    }
+}
---
+++ Rust_doc/target/debug/deps/traits-19285965b40f7ce8.d
@@ -0,0 +1,5 @@
+C:\Users\91908\Rust\Rust-Documentation\Rust_doc\target\debug\deps\traits-19285965b40f7ce8.rmeta: src\bin\traits.rs
+
+C:\Users\91908\Rust\Rust-Documentation\Rust_doc\target\debug\deps\traits-19285965b40f7ce8.d: src\bin\traits.rs
+
+src\bin\traits.rs:

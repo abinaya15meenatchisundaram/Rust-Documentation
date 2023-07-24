9be029c72c0f0d23bb1e288938c9a78bda67472e
abinaya15meenatchisundaram <abinaya.meenatchisundharam@gmail.com>
2023-07-24 18:10:55 +0530

    array basic

---
+++ Rust_doc/src/bin/array_basic.rs
@@ -0,0 +1,13 @@
+#! [allow(unused)]
+use std::io;
+use std::fmt::Display;
+use std::io::{Write,BufReader,BufRead,ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
+fn main(){
+    let arry=[1,2,3,4,5,6,7,8];
+    println!("1st: {}",arry[0]);
+    println!("Length:{}",arry.len());  
+
+}
